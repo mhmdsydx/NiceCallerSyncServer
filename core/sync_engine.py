3e7eea1bf142b45b1c6c2457671a308980827b50
mhmdsydx <mhmdsydx@gmail.com>
2026-07-02 01:39:24 +0300

    Update sync_engine.py

--- core/sync_engine.py
+++ core/sync_engine.py
@@ -1,1 +1,43 @@
+from database import Database
+from normalizer import normalize
+from logger import info, error
 
+from config import CACHE_DB
+from config import CALL_DB
+
+import json
+
+from pathlib import Path
+
+
+class SyncEngine:
+
+    def __init__(self):
+
+        self.cache = Database(CACHE_DB)
+
+        self.call = Database(CALL_DB)
+
+        self.phone_index = {}
+
+        self.label_index = {}
+
+        self.entry_index = {}
+
+        self.extension_id = None
+
+        self.next_phone_id = 1
+
+        self.next_label_id = 1
+
+        self.next_entry_id = 1
+
+        self.stats = {
+
+            "added":0,
+
+            "updated":0,
+
+            "skipped":0
+
+        }
