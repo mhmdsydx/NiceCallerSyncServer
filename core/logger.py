815dcc974ef4ccc0d049d13daa6d841e64ca5464
mhmdsydx <mhmdsydx@gmail.com>
2026-07-02 01:28:42 +0300

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,1 +1,24 @@
+import logging
+from config import LOG_FILE
 
+logger = logging.getLogger("NiceCallerSync")
+
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(message)s"
+)
+
+file_handler = logging.FileHandler(LOG_FILE)
+
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
+def info(msg):
+    logger.info(msg)
+
+
+def error(msg):
+    logger.error(msg)
